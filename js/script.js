95b4af4ab3eff64869edfe12838398f37a38b26b
radshodam <mohamadzareai@gmail.com>
2020-08-12 20:20:18 +0430

    countinue repository for learning js ;)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,61 @@
+var num1 = 0;
+var num2 = 0;
+var sign = null;
+
 function setValue(num) {
     //debugger;
     var old = document.getElementById('result').value;
     var res = null;
-    //!==
-    // ==
-    // ===
-    // <=
-    // >=
 
     if (old !== '0') {
+        //debugger;
         res = old + num;
     } else {
 
         res = num;
     }
 
-    document.getElementById('result').value = res;
+    document.getElementById('result').value = String(res);
 
+}
+
+function keepValue(alamat) {
+    sign = alamat;
+
+    debugger;
+    if (sign === '+' && num1 !== 0 && num2 !== 0) {
+        num1 = parseFloat(num1) + parseFloat(num2);
+        document.getElementById('result').value = num1;
+    }
+
+    num1 = document.getElementById('result').value;
+    document.getElementById('result').value = "";
+    console.log(num1);
+}
+
+function showResult() {
+    num2 = document.getElementById('result').value;
+
+    if (sign === '+') {
+        var final = parseFloat(num1) + parseFloat(num2);
+    } else if (sign === '-') {
+        var final = parseFloat(num1) - parseFloat(num2);
+    } else if (sign === '*') {
+        var final = parseFloat(num1) * parseFloat(num2);
+    } else if (sign === '/') {
+        var final = parseFloat(num1) / parseFloat(num2);
+    }
+
+
+    document.getElementById('result').value = final;
+
+    num1 = 0;
+    num2 = 0;
+}
+
+function cls() {
+    num1 = 0;
+    num2 = 0;
+
+    document.getElementById('result').value = "0";
 }
